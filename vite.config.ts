aa8462e34fd51dfb971a09cd23fc1dcde178c35b
Marcus Hellberg <marcus@vaadin.com>
2021-11-11 06:36:45 -0800

    don't exclude lit

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,7 @@ import dsv from '@rollup/plugin-dsv';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: /^lit/,
-    },
-  },
+  build: {},
   plugins: [
     //VitePWA({}),
     dsv(),
